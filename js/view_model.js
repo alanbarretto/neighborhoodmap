722af95c0ead146c5c037ca93528d85db4af9cac
Alan Barretto <alanbarretto@gmail.com>
2018-03-09 23:32:43 -0800

    add comments

--- js/view_model.js
+++ js/view_model.js
@@ -20,11 +20,13 @@
 
 		self.nationalParks = ko.observableArray([]);
 
+		//push each parks object into the nationalParks observable array
 		parks.forEach(function(data){
 			self.nationalParks.push(new Parks(data));
 		});
 
-
+		//when a selection is clicked from the dropdown menu, this function
+		//will trigger the marker associated with it as if the marker was clicked
 		self.selection.subscribe(function(newValue){
 			for (var i=0; i<parks.length;i++){
 				if (parks[i].title == newValue){
@@ -35,7 +37,8 @@
 			}
 			
 		})
-
+		//when a park name is clicked from the list on the left of the page, 
+		//this funcion will trigger the associated marker as if it were clicked
 		self.linkToMarker = function(park_obj){
 
 
